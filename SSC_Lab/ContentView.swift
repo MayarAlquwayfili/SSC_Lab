e0767cecbcaea13d579d0d3569da9d13ac6b49f3
Mayar <Mayar>
2026-02-09 23:00:37 +0300

    Initial Commit

---
+++ SSC_Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SSC_Lab
+//
+//  Created by yumii on 09/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SSC_Lab/SSC_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SSC_LabApp.swift
+//  SSC_Lab
+//
+//  Created by yumii on 09/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct SSC_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
